b87e1e76d42bfb496c63ef41cc5af1bfc01004c0
nasnju <nasnju@gmail.com>
2024-06-07 16:36:46 +0900

    sixth commit

--- src/main/java/controller/NewWindowController.java
+++ src/main/java/controller/NewWindowController.java
@@ -8,10 +8,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.CheckBoxTableCell;
 
 public class NewWindowController {
 	private Book_CopyDAO bookCopyDAO;
+	private ObservableList<Book_Copy> bookCopies;
+	
 	@FXML
 	private TextField tfGreatTitle;
 	@FXML
@@ -25,19 +26,33 @@ public class NewWindowController {
 	@FXML
 	private TableColumn<Book_Copy, Boolean> checkBoxColumn;
 	
+	/**
+	 * @author 나선주
+	 * @param rowData
+	 */
+	
 	public void initData(Book rowData) {
 		// TODO Auto-generated method stub
 		tfGreatTitle.setText(rowData.getBName());
 		tfAuthor.setText(rowData.getAuthor());
 		tfsmallTitle.setText(rowData.getBName());
 		tfISBN.setText(rowData.getIsbn());
+		System.out.println("isbn은 "+ rowData.getIsbn());
+//		
+//		this.bookCopyDAO.add(rowData);
+//		
+//		ObservableList<Book_Copy> bookcopys=bookCopyDAO.getBookCopysByISBN(rowData.getIsbn());
+//		System.out.println(bookcopys);
 		
-		ObservableList<Book_Copy> bookcopys;
-		bookcopys = bookCopyDAO.getBookCopysByISBN(rowData.getIsbn());
+//		tableView.setItems(bookcopys);
+//		checkBoxColumn.setCellFactory(CheckBoxTableCell.forTableColumn(checkBoxColumn));
+//		checkBoxColumn.setEditable(true);
 		
-		tableView.setItems(bookcopys);
-		checkBoxColumn.setCellFactory(CheckBoxTableCell.forTableColumn(checkBoxColumn));
-		checkBoxColumn.setEditable(true);
+		
+	}
+
+	public NewWindowController() {
+		super();
 	}
 
 }
